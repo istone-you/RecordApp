7a1e01952d2ad3bdabb91079a7385a93b56d168c
EC2 Default User <ec2-user@ip-172-31-37-164.ap-northeast-3.compute.internal>
2021-12-22 12:23:41 +0000

    edit gemfile and database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_150241) do
+ActiveRecord::Schema.define(version: 2021_12_16_150157) do
 
   create_table "folder_tags", force: :cascade do |t|
     t.integer "folder_id"
@@ -24,13 +24,16 @@ ActiveRecord::Schema.define(version: 2021_12_19_150241) do
   create_table "folders", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "count_sum"
+    t.integer "hour_sum"
     t.integer "money_sum"
     t.integer "minute_sum"
     t.integer "record_view", default: 0
+    t.integer "record_sum"
+    t.integer "done_view", default: 0
     t.integer "record_sort", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
@@ -49,16 +52,19 @@ ActiveRecord::Schema.define(version: 2021_12_19_150241) do
     t.text "coment"
     t.string "image"
     t.integer "folder_id"
+    t.integer "user_id"
     t.integer "money"
     t.integer "done", default: 1
     t.integer "goal_count"
     t.integer "minutes"
     t.integer "hours"
+    t.text "link"
+    t.string "youtube"
+    t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "link"
-    t.string "twitter"
     t.index ["folder_id"], name: "index_records_on_folder_id"
+    t.index ["user_id"], name: "index_records_on_user_id"
   end
 
   create_table "tag2s", force: :cascade do |t|
@@ -72,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_12_19_150241) do
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tags_on_user_id"
@@ -82,6 +89,8 @@ ActiveRecord::Schema.define(version: 2021_12_19_150241) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.integer "all_records_view", default: 0
+    t.integer "all_records_sort", default: 0
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
