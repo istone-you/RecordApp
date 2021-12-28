059b9584fe437e7d287c0648ce201ac468517116
EC2 Default User <ec2-user@ip-172-31-37-164.ap-northeast-3.compute.internal>
2021-12-28 23:02:28 +0000

    none

--- db/migrate/20211228230003_add_all_record_view_to_tag2s.rb
+++ db/migrate/20211228230003_add_all_record_view_to_tag2s.rb
@@ -1,5 +1,6 @@
 class AddAllRecordViewToTag2s < ActiveRecord::Migration[5.2]
   def change
-    add_column :tag2s, :all_records_view, :integer
+    add_column :tag2s, :all_records_view, :integer, default: 0
+    add_column :tag2s, :all_records_sort, :integer, default: 0
   end
 end
