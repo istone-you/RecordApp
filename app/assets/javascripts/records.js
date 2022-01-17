3b093623b064bbf3f6959f4f30c0b08c5ff6cd22
EC2 Default User <ec2-user@ip-172-31-36-72.ap-northeast-3.compute.internal>
2022-01-17 06:32:05 +0000

    微調整

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -5,3 +5,17 @@ $(document).ready(function(){
   });
 });
 })
+
+function previewImage(id) {
+  const target = this.event.target;     
+  const file = target.files[0];      
+  const reader = new FileReader();  
+  reader.onloadend = function() {   
+    preview.src = reader.result;     
+  };
+  if (file) {
+    reader.readAsDataURL(file);      
+  } else {
+    preview.src = "";
+  }
+}
